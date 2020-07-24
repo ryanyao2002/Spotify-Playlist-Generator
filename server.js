8ee12547bb85cfcbf89c6243232bb19e73956354
Ryan Yao <theryanyao@gmail.com>
2020-07-24 00:16:03 -0700

    test4

--- server.js
+++ server.js
@@ -142,6 +142,12 @@ app.get('/refresh_token', function(req, res) {
     }
   });
 });
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 console.log('Listening on 8888');
