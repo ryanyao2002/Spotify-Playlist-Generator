c1f51bd12dbed881f3ceb4598d86017693ea9cc1
Ryan Yao <theryanyao@gmail.com>
2020-07-23 23:43:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+const querystring = require('querystring');
 
 function App() {
+
+  const redirect_uri = "https://dashboard.heroku.com/apps/playlist-generator-for-spotify/callback"
+
+
+  const getToken = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const result = await axios('https://accounts.spotify.com/api/token', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')
+        },
+        data: querystring.stringify({
+          'grant_type' : 'authorization_code',
+          'code': urlParams.get('code'),
+          'redirect_uri' : redirect_uri
+        })
+      })
+
+      let data = await result;
+       return data.data.access_token;
+
+  }
+
+
+  function  createPlaylist (data, token){
+    console.log(data);
+      fetch("https://api.spotify.com/v1/me" ,{
+      headers: {
+        'Authorization' : 'Bearer ' + token
+      }
+    }).then(function(res){
+        return res.json();
+    }).then(function(res){
+      fetch(res.href +"/playlists", {
+        method: 'POST',
+        headers:{
+          'Authorization' : 'Bearer ' + token,
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+          'name' : 'New Random Playlist',
+        })
+      }).then(function(res){
+        return res.json();
+    }).then(function(res){
+      searchAndAddRelatedItems(res.id, token, data);
+    })
+    })
+  }
+
+
+  function searchAndAddRelatedItems(playlistIDURL, token, searchData){
+    let searchquery = ""+ searchData.items[0].name;
+
+    fetch("https://api.spotify.com/v1/search?q=artist:" + searchquery +"&type=track", {
+      headers:{
+        'Authorization': 'Bearer ' + token
+      } 
+    }).then(function(res){
+      return res.json();
+  }).then(function(res){
+    let itemsArr = res.tracks.items;
+    itemsArr = itemsArr.map(function(e){
+      return e.uri
+    })
+    console.log(itemsArr);
+
+    fetch("https://api.spotify.com/v1/playlists/"+ playlistIDURL + "/tracks", {
+        method:'POST',
+        headers:{
+          'Authorization': 'Bearer ' + token
+        },
+        body:JSON.stringify({
+          'uris': itemsArr
+        })
+      })
+  })
+  }
+
+  const  handleClick1 = async () =>{
+
+        const token = await getToken();
+          fetch("https://api.spotify.com/v1/me/top/artists?limit=1" ,{
+          headers: {
+            'Authorization' : 'Bearer ' + token
+          }
+        }).then(function(res){
+            return res.json();
+        }).then(function(res){
+          createPlaylist(res, token);
+        })
+      }
+
+  
+
+
+
+  
+  function handleClick(){
+    window.location = 'https://dashboard.heroku.com/apps/playlist-generator-for-spotify/login';  
+  }
+
+
+  useEffect(function(){
+    if(new URLSearchParams(window.location.search).get('code') !== null){
+      handleClick1();
+    }
+  }, []);
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <h1> Playlist Generator for Spotify</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click to be able to generate a randomized track based on your most listened to genres!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button  className="btn btn-success" onClick={handleClick}> Generate New Playlist!</button>
     </div>
   );
 }
